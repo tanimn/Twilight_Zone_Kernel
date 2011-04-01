e32bf18ced4d2e3ba7e4ef735280d46e50a0c50f
tanimn <tanimn009@gmail.com>
2011-04-01 03:57:33 -0500

    Undervolt Display care of richardtrip and koxudaxi.

--- Kernel/arch/arm/mach-s5pv210/powergating.c
+++ Kernel/arch/arm/mach-s5pv210/powergating.c
@@ -200,9 +200,9 @@ int s5p_pmic_gating(unsigned int power_domain, unsigned int on_off)
 		} else {
 			// power off
 			max8998_ldo_disable_direct(MAX8998_LDO7);	
-			max8998_ldo_set_voltage_direct(MAX8998_LDO7,1800000, 1800000);
+			max8998_ldo_set_voltage_direct(MAX8998_LDO7,1600000, 1600000);
 			max8998_ldo_disable_direct(MAX8998_LDO17);
-			max8998_ldo_set_voltage_direct(MAX8998_LDO17,3000000,3000000);
+			max8998_ldo_set_voltage_direct(MAX8998_LDO17,2700000,2700000);
 		}
 		break;
 
